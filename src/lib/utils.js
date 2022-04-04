db590bba96c6165d4ad43f2060bc3dbc28c9c04c
Astroth1984 <elmounir.othmane666@gmail.com>
2022-04-04 02:49:51 +0200

    Snake Game

---
+++ src/lib/utils.js
@@ -0,0 +1,42 @@
+import { useEffect, useRef } from 'react';
+
+/*
+ *  Generate a random number from an interval
+ */
+export function randomIntFromInterval(min, max) {
+    // min & max included
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+// Copied from https://overreacted.io/making-setinterval-declarative-with-react-hooks/
+export function useInterval(callback, delay) {
+    const savedCallback = useRef();
+
+    // Remember the latest callback.
+    useEffect(() => {
+        savedCallback.current = callback;
+    }, [callback]);
+
+    // Set up the interval.
+    useEffect(() => {
+        function tick() {
+            savedCallback.current();
+        }
+        if (delay !== null) {
+            let id = setInterval(tick, delay);
+            return () => clearInterval(id);
+        }
+    }, [delay]);
+}
+
+export function reverseLinkedList(head) {
+    let previousNode = null;
+    let currentNode = head;
+    while (currentNode !== null) {
+        const nextNode = currentNode.next;
+        currentNode.next = previousNode;
+        previousNode = currentNode;
+        currentNode = nextNode;
+    }
+    return previousNode;
+}
